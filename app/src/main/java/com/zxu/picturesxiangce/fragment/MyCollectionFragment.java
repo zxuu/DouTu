4088b4518a1c24fb0842292adf536d4195aa3330
zxu <z18255713582@outlook.com>
2019-06-22 11:42:59 +0800

    videoDetailStart2

--- app/src/main/java/com/zxu/picturesxiangce/fragment/MyCollectionFragment.java
+++ app/src/main/java/com/zxu/picturesxiangce/fragment/MyCollectionFragment.java
@@ -1,13 +1,20 @@
 package com.zxu.picturesxiangce.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zxu.picturesxiangce.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,15 +22,11 @@ import com.zxu.picturesxiangce.R;
 public class MyCollectionFragment extends Fragment {
 
 
-    public MyCollectionFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_my_collection, container, false);
     }
 
